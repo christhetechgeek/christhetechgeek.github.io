1ca01cb3ab8a73fb87d36e61997c8fd517e1c0d3
christhetechgeek <68786650+christhetechgeek@users.noreply.github.com>
2024-03-07 17:54:03 -0500

    added projects folder
    commit

---
+++ gaming/super_puppy/script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'super_puppy.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 30;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  //ctx.fillrect(100,50,100,100)
+  //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames === 0) {
+  if (frameX < 6) frameX++;
+  else frameX = 0;
+    }
+
+    gameFrame++;
+  requestAnimationFrame(animate);
+    
+
+};
+
+animate();
